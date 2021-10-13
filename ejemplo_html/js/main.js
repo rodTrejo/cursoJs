e857dce712acb7e75003f76b2af4339ad92e5c69
rodTrejo <rodrigo.noe@hotmail.com>
2021-10-13 13:45:58 -0500

    complemento

---
+++ ejemplo_html/js/main.js
@@ -0,0 +1,9 @@
+
+main = function(){
+
+    const div = document.getElementById('inicio');
+    const elm = document.createElement("strong");
+    elm.innerHTML = "Hola Mundo";
+    div.appendChild(elm);
+
+};
